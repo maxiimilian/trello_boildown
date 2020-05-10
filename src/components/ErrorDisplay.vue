161ae76cac4fa384bb7b7c005accf51a0cac54f1
Maximilian Pierzyna <mail@maximilian-pierzyna.de>
2020-05-10 20:22:57 +0200

    Move Auth component into view. Add Error display

---
+++ src/components/ErrorDisplay.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-if="errors.length > 0" class="ui visible error message">
+    <ul>
+      <li v-for="(e, index) in errors" v-bind:key="index">{{ e }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorDisplay',
+  props: {
+    errors: Array
+  }
+}
+</script>
--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,16 +1,65 @@
 <template>
-    <div>
-        <TrelloAuth />
+    <div id="trello_auth" class="ui form">
+        <ErrorDisplay v-bind:errors="errors" />
+        <div class="field">
+            <label class="label">
+                Trello Token
+                <input class="input" type="text" name="trello_auth_token" v-model.lazy="token"/>
+            </label>
+        </div>
+        <div class="field">
+            <label class="label">
+                Trello Key
+                <input class="input" type="text" name="trello_auth_key" v-model.lazy="key"/>
+            </label>
+        </div>
+        <button class="ui primary button" v-on:click="get_my_boards">Connect</button>
     </div>
 </template>
 
 <script>
-import TrelloAuth from '../components/TrelloAuth.vue'
+import ErrorDisplay from '../components/ErrorDisplay.vue'
 
 export default {
   name: 'AuthView',
   components: {
-    TrelloAuth
+    ErrorDisplay
+  },
+  data () {
+    return {
+      errors: []
+    }
+  },
+  computed: {
+    connected () {
+      return this.$store.state.trello_auth.connected
+    },
+    token: {
+      get () {
+        return this.$store.state.trello_auth.token
+      },
+      set (value) {
+        this.$store.commit('set_auth_token', value)
+      }
+    },
+    key: {
+      get () {
+        return this.$store.state.trello_auth.key
+      },
+      set (value) {
+        this.$store.commit('set_auth_key', value)
+      }
+    }
+  },
+  methods: {
+    get_my_boards () {
+      this.$store.dispatch('get_my_boards').then(() => {
+        this.errors = []
+        this.$router.push('list')
+      }).catch(e => {
+        this.errors.push(e)
+      })
+    }
   }
 }
 </script>
